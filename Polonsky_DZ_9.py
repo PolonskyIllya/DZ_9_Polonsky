c19f6f6a31f64db25c1394430c9b6c6a0ba256d5
PolonskyIllya <153635933+PolonskyIllya@users.noreply.github.com>
2023-12-17 15:31:37 +0300

    Add files via upload

---
+++ Polonsky_DZ_9.py
@@ -0,0 +1,32 @@
+import json
+# Задание №1
+
+f = b'r\xc3\xa9sum\xc3\xa9'
+u = f.byte_string.decode('utf-8')
+print("Декодированная строка:", f)
+
+# Задание №2
+# stroka1 = input()
+# stroka2 = input()
+# stroka3 = input()
+# stroka4 = input()
+# w = open('text_hw.txt','w')
+# w.write(f'{stroka1}\n{stroka2}\n')
+# w.close()
+# w = open('text_hw.txt','a')
+# w.write(f'{stroka1}\n{stroka4}')
+# w.close()
+
+# Задание №3
+
+spisok = {
+#     742388: ("Ilya", 35),
+#     101213: ("Evgen", 75),
+#     171819: ("Yura", 13),
+#     242424: ("Svetlana", 15),
+#     181818: ("Dima", 18),
+#     777777: ("Max", 17),
+# }
+# with open("spisok.json", "w") as file:
+#     json.dump(spisok, file)
+
